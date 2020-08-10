5d82eebab439429eb9fbd0cea0a64f9307356c8c
Waqar Akbar <waqarakbar89@gmail.com>
2020-08-10 18:02:51 +0500

    1.6: unicafe step1 completed

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -7,9 +7,44 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const goodClick = () => {
+    setGood(good+1)
+  }
+
+  const neutralClick = () => {
+    setNeutral(neutral+1)
+  }
+
+  const badClick = () => {
+    setBad(bad+1)
+  }
+
   return (
     <div>
-      code here
+      <h1>Give geedback</h1>
+      <Button clickEvent={goodClick} text='good' />
+      <Button clickEvent={neutralClick} text='neutral' />
+      <Button clickEvent={badClick} text='bad' />
+
+      <h2>Statistics</h2>
+      <DisplayStatistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+
+const Button = ({clickEvent, text}) => {
+  return (
+    <button onClick={clickEvent}>{text}</button>
+  )
+}
+
+const DisplayStatistics = ({good, neutral, bad}) => {
+  return (
+    <div>
+      <p>Good {good}</p>
+      <p>Neutral {neutral}</p>
+      <p>Bad {bad}</p>
     </div>
   )
 }
